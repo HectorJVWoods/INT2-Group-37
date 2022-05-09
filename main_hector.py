d566b326641270503076ddc23b3f5d63e025af50
HectorJVWoods <hjvw500@york.ac.uk>
2022-05-09 05:50:45 +0100

    Added a function to optimize batch size

--- main_hector.py
+++ main_hector.py
@@ -42,18 +42,25 @@ augmented_data = torchvision.datasets.CIFAR10(root='./data',
 
 training_data = torch.utils.data.ConcatDataset([training_data, augmented_data])
 
-train_loader = torch.utils.data.DataLoader(training_data,
-                                           batch_size=training_batch,
-                                           shuffle=True,
-                                           num_workers=2)
 test_data = torchvision.datasets.CIFAR10(root='./data',
                                          train=False,
                                          download=not already_downloaded,
                                          transform=transform)
-test_loader = torch.utils.data.DataLoader(test_data,
-                                          batch_size=test_batch,
-                                          shuffle=False,
+
+
+def init_data_sets(train_batch_size, test_batch_size):
+    test_d = torch.utils.data.DataLoader(test_data,
+                                         batch_size=test_batch_size,
+                                         shuffle=False,
+                                         num_workers=2)
+    train_d = torch.utils.data.DataLoader(training_data,
+                                          batch_size=train_batch_size,
+                                          shuffle=True,
                                           num_workers=2)
+    return train_d, test_d
+
+
+train_loader, test_loader = init_data_sets(train_batch_size=128, test_batch_size=1000)
 
 
 class Net(nn.Module):
@@ -69,32 +76,44 @@ class Net(nn.Module):
         self.layer7 = nn.Linear(128, 64)
         self.layer8 = nn.Linear(64, 10)
         self.dropout = nn.Dropout(0.15)
+        self.batchnorm1 = nn.BatchNorm2d(64)
+        self.batchnorm2 = nn.BatchNorm2d(128)
+        self.batchnorm3 = nn.BatchNorm2d(256)
+        self.batchnorm4 = nn.BatchNorm2d(512)
+        self.batchnorm5 = nn.BatchNorm2d(512)
 
     def forward(self, x):
         x = self.layer1(x)
         x = F.relu(x)
+        x = self.batchnorm1(x)
         x = self.pool(x)
+        x = self.dropout(x)
         x = self.layer2(x)
         x = F.relu(x)
+        x = self.batchnorm2(x)
         x = self.pool(x)
+        x = self.dropout(x)
         x = self.layer3(x)
         x = F.relu(x)
+        x = self.batchnorm3(x)
         x = self.layer4(x)
         x = F.relu(x)
+        x = self.batchnorm4(x)
         x = self.layer5(x)
         x = F.relu(x)
+        x = self.batchnorm5(x)
         x = self.layer5(x)
         x = F.relu(x)
+        x = self.batchnorm5(x)
         x = self.layer5(x)
         x = F.relu(x)
+        x = self.batchnorm5(x)
         x = self.pool(x)
         x = torch.flatten(x, 1)  # flatten all dimensions except batch
-        x = self.dropout(x)
         x = self.layer6(x)
         x = F.relu(x)
         x = self.layer7(x)
         x = F.relu(x)
-        x = self.dropout(x)
         x = self.layer8(x)
         return x
 
@@ -155,7 +174,7 @@ def train_for_n_minutes(n, net, loss_fn, optimizer, file_path, show_graph):
         print(f"Saving these parameters to {file_path}")
         save_model(file_path, best_params_so_far)
         plot_error_rates(training_error, test_error)
-    return best_params_so_far, lowest_error, best_epoch
+    return best_params_so_far, lowest_error, best_epoch, training_error, test_error
 
 
 def plot_error_rates(training_error, test_error):
@@ -166,15 +185,73 @@ def plot_error_rates(training_error, test_error):
     plt.show()
 
 
+def plot_multiple_error_rates(tr_errors, te_errors, labels, label_name):
+    if len(tr_errors) != len(te_errors) or len(tr_errors) != len(labels):
+        print("Dim. are not the same size!")
+        return
+    for i in range(len(tr_errors)):
+        training_error = tr_errors[i]
+        test_error = te_errors[i]
+        l = labels[i]
+        x = [x for x in range(len(training_error))]
+        plt.plot(x, training_error, label="train (" + label_name + "=" + str(l) + ")")
+        plt.plot(x, test_error, label="test (" + label_name + "=" + str(l) + ")")
+    plt.legend()
+    plt.show()
+
+
 def train_for_n_hours(n, net, loss_fn, optimizer, file_path):
     train_for_n_minutes(n * 60, net, loss_fn, optimizer, file_path)
 
 
-def optimize_learning_rates(mins_per_train_cycle, file_path, loss_fn, optim):
-    learning_rates = [ 0.005, 0.001, 0.0005, 0.0001, 0.00005, 0.00001]
+def optimize_batches(mins_per_train_cycle, file_path, loss_fn, optim, learn_rate):
+    global train_loader, test_loader
+    batch_sizes = [8, 16, 32, 64, 128, 256, 512, 1024]
     best_params = None
     lowest_error = math.inf
     test_errors = []
+    test_graphs = []
+    train_graphs = []
+    best_epoch = -1
+    best_bs = None
+    for BS in batch_sizes:
+        print("----------------------------------")
+        print("----------------------------------")
+        print(f"Trying batch size: {BS}")
+        print("----------------------------------")
+        print("----------------------------------")
+        train_loader, test_loader = init_data_sets(train_batch_size=BS, test_batch_size=1000)
+        t_net = Net().to(device)
+        params, test_error, epoch, train_graph, test_graph = train_for_n_minutes(
+            mins_per_train_cycle, t_net, loss_fn(), optim(lr=learn_rate, params=t_net.parameters()), "", False)
+        test_errors.append(test_error)
+        train_graphs.append(train_graph)
+        test_graphs.append(test_graph)
+        if test_error < lowest_error:
+            print("New record for test error!")
+            best_params = net.state_dict()
+            lowest_error = test_error
+            best_epoch = epoch
+            best_bs = BS
+    print("----------------------------------")
+    print("----------------------------------")
+    print(f"Optimization complete. The optimal batch size was {best_bs}, with a test error of {lowest_error}, "
+          f"at an optimal epoch of {best_epoch}")
+    print(f"Saving the best model to {file_path}")
+    print(test_errors)
+    save_model(file_path, best_params)
+    plot_multiple_error_rates(train_graphs, test_graphs, batch_sizes, "bs")
+    return best_bs, best_params
+
+
+def optimize_learning_rates(mins_per_train_cycle, file_path, loss_fn, optim):
+    #learning_rates = [0.005, 0.001, 0.0005]
+    learning_rates = [0.005, 0.001, 0.0005, 0.0001, 0.00005, 0.00001]
+    best_params = None
+    lowest_error = math.inf
+    test_errors = []
+    test_graphs = []
+    train_graphs = []
     best_epoch = -1
     best_lr = None
     for LR in learning_rates:
@@ -184,9 +261,11 @@ def optimize_learning_rates(mins_per_train_cycle, file_path, loss_fn, optim):
         print("----------------------------------")
         print("----------------------------------")
         t_net = Net().to(device)
-        params, test_error, epoch = train_for_n_minutes(mins_per_train_cycle, t_net,
-                                                        loss_fn(), optim(lr=LR, params=t_net.parameters()), "", False)
+        params, test_error, epoch, train_graph, test_graph = train_for_n_minutes(
+            mins_per_train_cycle, t_net, loss_fn(), optim(lr=LR, params=t_net.parameters()), "", False)
         test_errors.append(test_error)
+        train_graphs.append(train_graph)
+        test_graphs.append(test_graph)
         if test_error < lowest_error:
             print("New record for test error!")
             best_params = net.state_dict()
@@ -200,6 +279,8 @@ def optimize_learning_rates(mins_per_train_cycle, file_path, loss_fn, optim):
     print(f"Saving the best model to {file_path}")
     print(test_errors)
     save_model(file_path, best_params)
+    plot_multiple_error_rates(train_graphs, test_graphs, learning_rates, "lr")
+    return best_lr, best_params
 
 
 def test(net):
@@ -221,10 +302,10 @@ if __name__ == "__main__":
     device1 = torch.device('cpu')
     print('Device:', device)
 
-    #net = Net().to(device)
-    net = load_model("model1").to(device)
-    train_for_n_minutes(1, net, loss_fn=nn.CrossEntropyLoss(),
-                        optimizer=torch.optim.Adam(lr=0.0005, params=net.parameters()), file_path="model1",
-                        show_graph=True)
+    net = Net().to(device)
+    # net = load_model("model1").to(device)
+    #train_for_n_minutes(1, net, loss_fn=nn.CrossEntropyLoss(),
+    #                    optimizer=torch.optim.Adam(lr=0.0005, params=net.parameters()), file_path="model1",
+     #                   show_graph=True)
 
-    # optimize_learning_rates(45, "model2", nn.CrossEntropyLoss, torch.optim.Adam)
+    optimize_batches(60, "model2", nn.CrossEntropyLoss, torch.optim.Adam, 0.0005)
